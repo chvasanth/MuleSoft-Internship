f7b1f02c757129b4947df7a26197065baf4b4221
chvasanth <55314748+chvasanth@users.noreply.github.com>
2022-01-13 12:24:26 +0530

    Add files via upload

---
+++ NJC_Assignment/src/DbConnection.java
@@ -0,0 +1,22 @@
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+
+public class DbConnection {
+    public static Connection connect(){
+        Connection con = null;
+        try {
+            Class.forName("org.sqlite.JDBC");
+            con = DriverManager.getConnection("jdbc:sqlite:javaDatabaseTutorials.db");
+            System.out.println("connected");
+        } catch (ClassNotFoundException | SQLException e) {
+           System.out.println(e+"");
+        }
+
+    
+
+    return con;
+    }
+    
+}
